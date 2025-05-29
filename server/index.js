e74bd8a43c8eb5f3ff936394c7614ea9fa543609
kirtichaprana-07 <kchaprana@ex2india.com>
2025-05-29 10:53:07 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// server/index.js
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
